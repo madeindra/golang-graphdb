45b86e33fbba9d8b4e54812278e07444d0f24a35
Made Indra <made.indra@protonmail.com>
2023-02-10 18:40:14 +0700

    fix code

--- main.go
+++ main.go
@@ -1,30 +1,30 @@
 package main
 
 import (
+	"context"
 	"fmt"
 
-	"github.com/neo4j/neo4j-go-driver/neo4j"
+	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
 )
 
 func main() {
+	// create Context
+	ctx := context.Background()
+
 	// Connect to Neo4j
-	driver, err := neo4j.NewDriver("bolt://localhost:7687", neo4j.BasicAuth("neo4j", "password", ""))
+	driver, err := neo4j.NewDriverWithContext("neo4j://localhost:7687", neo4j.BasicAuth("neo4j", "test", ""))
 	if err != nil {
 		fmt.Println("Error connecting to Neo4j:", err)
 		return
 	}
-	defer driver.Close()
+	defer driver.Close(ctx)
 
 	// Create a session
-	session, err := driver.Session(neo4j.AccessModeWrite)
-	if err != nil {
-		fmt.Println("Error creating session:", err)
-		return
-	}
-	defer session.Close()
+	session := driver.NewSession(ctx, neo4j.SessionConfig{AccessMode: neo4j.AccessModeWrite})
+	defer session.Close(ctx)
 
 	// Create a node
-	createResult, err := session.Run("CREATE (p:Person {name: $name})", map[string]interface{}{"name": "John Doe"})
+	createResult, err := session.Run(ctx, "CREATE (p:Person {name: $name})", map[string]interface{}{"name": "John Doe"})
 	if err != nil {
 		fmt.Println("Error creating node:", err)
 		return
@@ -35,7 +35,7 @@ func main() {
 	}
 
 	// Read a node
-	readResult, err := session.Run("MATCH (p:Person {name: $name}) RETURN p", map[string]interface{}{"name": "John Doe"})
+	readResult, err := session.Run(ctx, "MATCH (p:Person {name: $name}) RETURN p", map[string]interface{}{"name": "John Doe"})
 	if err != nil {
 		fmt.Println("Error reading node:", err)
 		return
@@ -44,14 +44,14 @@ func main() {
 		fmt.Println("Error in read result:", readResult.Err())
 		return
 	}
-	if readResult.Next() {
-		fmt.Println("Read node:", readResult.Record().GetByIndex(0).(neo4j.Node).Props())
+	if readResult.Next(ctx) {
+		fmt.Println("Read node:", readResult.Record().Values[0].(neo4j.Node).GetProperties())
 	} else {
 		fmt.Println("Node not found")
 	}
 
 	// Update a node
-	updateResult, err := session.Run("MATCH (p:Person {name: $name}) SET p.age = $age", map[string]interface{}{"name": "John Doe", "age": 30})
+	updateResult, err := session.Run(ctx, "MATCH (p:Person {name: $name}) SET p.age = $age", map[string]interface{}{"name": "John Doe", "age": 30})
 	if err != nil {
 		fmt.Println("Error updating node:", err)
 		return
@@ -62,7 +62,7 @@ func main() {
 	}
 
 	// Delete a node
-	deleteResult, err := session.Run("MATCH (p:Person {name: $name}) DELETE p", map[string]interface{}{"name": "John Doe"})
+	deleteResult, err := session.Run(ctx, "MATCH (p:Person {name: $name}) DELETE p", map[string]interface{}{"name": "John Doe"})
 	if err != nil {
 		fmt.Println("Error deleting node:", err)
 		return
